87f908ed48373cec484beccb2eaee76260100bea
Eduardo Correia <eduardo.junior@solutis.com.br>
2019-09-05 09:14:33 -0300

    Added junkfood resource, models and its service with findAll and saveOne methods

---
+++ src/main/java/model/JunkFood.java
@@ -0,0 +1,30 @@
+package model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "junk_food")
+@NamedQuery(name = "JunkFoods.findAll",
+        query = "SELECT f FROM JunkFood f ORDER BY f.name",
+        hints = @QueryHint(name = "org.hibernate.cacheable", value = "true") )
+public class JunkFood extends model.abstracts.Entity {
+    @Column(length = 40, unique = true)
+    private String name;
+
+    public JunkFood() {
+        this.setRegistrationDate(LocalDate.now());
+    }
+    public JunkFood(String name) {
+        this.setRegistrationDate(LocalDate.now());
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/model/abstracts/Entity.java
@@ -0,0 +1,47 @@
+package model.abstracts;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@MappedSuperclass
+public class Entity extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate registrationDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity entity = (Entity) o;
+        return id.equals(entity.id) &&
+                registrationDate.equals(entity.registrationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, registrationDate);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
