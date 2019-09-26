b4d29731f9f4b99051e4cd1af4932cd49dab9aeb
Fernando <fernando.oliveira@solutis.com.br>
2019-09-26 18:18:33 -0300

    Config the new database and edit the dockerfile
    Config the new database and edit the dockerfile  to do not uses cache files i guess

--- src/test/java/serviceTest/SprintServiceTest.java
+++ src/test/java/serviceTest/SprintServiceTest.java
@@ -1,16 +1,74 @@
 package serviceTest;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
+
 import service.SprintService;
-import static io.restassured.RestAssured.given;
+
 import javax.inject.Inject;
 
-@QuarkusTest
+
 public class SprintServiceTest {
 
     @Inject
     SprintService service;
+//package serviceTest;
+//
+//import model.ChocoBox;
+//import org.hibernate.query.Query;
+//import org.junit.Assert;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.mockito.runners.MockitoJUnitRunner;
+//import service.ChocoBoxService;
+//
+//import javax.persistence.EntityManager;
+//import java.lang.reflect.Array;
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import static org.mockito.Matchers.*;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//    @RunWith(MockitoJUnitRunner.class)
+//    class ChocoBoxServiceTest {
+//
+//        @Mock
+//        EntityManager entityManager = mock(EntityManager.class);
+//
+//
+//        @InjectMocks
+//        ChocoBoxService service = new ChocoBoxService(this.entityManager);
+//
+//        @BeforeEach
+//        void setUp() throws Exception{
+//            MockitoAnnotations.initMocks(this);
+//        }
+//
+//        @Test
+//        public void test(){
+//
+//            LocalDate date;
+//            date = LocalDate.now();
+//            ChocoBox[] chocoBoxes = new ChocoBox[1];
+//            ChocoBox choco = new ChocoBox("alura", "Não sabe mockar", 1L, true,  date.plusDays(15));
+//            chocoBoxes[0] = choco;
+//            List<ChocoBox> chocoBoxList = new ArrayList<>();
+//            chocoBoxList.add(choco);
+//            Query query = mock(Query.class);
+//            query.setParameter(0, choco);
+//            when(entityManager.createNativeQuery("SELECT f FROM ChocoBox f ORDER BY f.name", ChocoBox.class)).thenReturn(query);
+//            //when(query.getResultList()).thenReturn(Arrays.asList(chocoBoxes));
+//            //when(service.findAll()).thenReturn(Arrays.asList(chocoBoxes));
+//            System.out.println(query.getResultList() + "Teste");
+//            Assert.assertEquals(service.findAll(), chocoBoxList);
+//
+//        }
+//    }
 
 
 }
